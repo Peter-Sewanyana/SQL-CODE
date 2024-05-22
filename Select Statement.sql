edb8745a05d7039b5b0f47339e2633db94e5ac0b
Peter-Sewanyana <sewapetero@gmail.com>
2024-05-22 17:55:51 +0300

    Add files via upload

---
+++ Select Statement.sql
@@ -0,0 +1,15 @@
+select	*
+from employee_demographics;
+
+
+
+select	
+first_name,
+last_name,
+birth_date,
+age,
+(age +10) *10
+from employee_demographics;
+
+select distinct first_name,gender
+from employee_demographics;
